ff03d03770831427a77a59cc63d825de90bbb97a
Alexey <alexey.derenko@gmail.com>
2019-03-13 23:28:45 +0200

    JS timer example

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+ let deadline = '2019-03-13';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
